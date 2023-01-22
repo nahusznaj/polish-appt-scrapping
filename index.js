46a10fd712896c023891ccf2517b792c052cf4da
Nahuel <nsznajderhaus@asapp.com>
2023-01-22 16:35:24 -0300

    use env for URL

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import puppeteer from 'puppeteer';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+
+
 (async () => {
   const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
   //await page.goto('http://localhost:3000',{waitUntil: "domcontentloaded"});
-  await page.goto('///', { waitUntil: 'networkidle0'});
+  await page.goto(process.env.MYURL, { waitUntil: 'networkidle0'});
   // ALWAYS USE return INSIDE evaluate BECAUSE IT HAPPENS IN THE DOM AND WE NEED TO RETURN IT TO puppeteer
    const img_text = await page.evaluate(() => { 
         const img_text = document.getElementsByClassName("mb-16")[1].firstElementChild.src.split(',')[1];
